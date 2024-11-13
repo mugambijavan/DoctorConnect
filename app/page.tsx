cf12a79657bb132bc11f6b42a24d183aa85c051c
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-11-13 23:57:11 +0300

    Refactor layout and styles; update homepage greeting, enhance Tailwind CSS configuration, and add utility functions

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline">Welcome to my doctorconnect</h1>
+      <h1 className="text-3xl underline">Welcome to  doctorconnect</h1>
     </div>  
   )
 }

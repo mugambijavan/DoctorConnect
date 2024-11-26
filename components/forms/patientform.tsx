1c7d986ace1da65109f484dc4d9a7c2ee2dbeb71
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-11-26 19:09:52 +0300

    Add custom form components and integrate form validation with Zod

--- components/forms/patientform.tsx
+++ components/forms/patientform.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import customformfield from "../customformfield"
+    
+    const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+    })
+    
+    const patientform =() => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+        username: "",
+        },
+    })
+    
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+    return (
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+            <section className="mb-12 space-y-4">
+                <h1 className="text-24-semibold">Hello, Welcome to DoctorConnect</h1>
+                <p className="text-sm text-gray-700">
+                    We're here to help you find the best doctor for your needs.
+                </p>
+            </section>
 
-const patientform = () => {
-  return (
-    <div>patientform</div>
-  )
+            <customformfield />
+
+            <Button type="submit">Submit</Button>
+        </form>
+        </Form>
+    )
 }
 
 export default patientform

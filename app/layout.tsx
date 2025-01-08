6f32ff3d179bedb08dba8efcce2e9c9760b64808
Javoh <117713938+mugambijavan@users.noreply.github.com>
2025-01-08 12:26:30 +0300

    Update global styles, upgrade dependencies, and refactor layout for improved font handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "./globals.css";
-import {cn} from '@/lib/utils';
-import { ThemeProvider } from "@/components/themeprovider";
+import { cn } from "@/lib/utils";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const fontSans = FontSans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Doctor Connect",
@@ -26,16 +21,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn ('min-h-screen bg-dark-300 font-sans antialiased')}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            >
-            {children}
-          </ThemeProvider>
+    <html lang="en" className="dark" data-theme="dark">
+      <head>
+        
+      </head>
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        
+          {children}
       </body>
     </html>
   );

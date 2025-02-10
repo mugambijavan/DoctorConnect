dbe237c47c8abffa37bdf5b1b81c98af2597d073
Javoh <117713938+mugambijavan@users.noreply.github.com>
2025-02-10 18:14:33 +0300

    Add PatientForm component, SVG icons, and IDE configuration files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div> 
-      <h1 className="text-3xl text-white underline">Hello world</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen"> 
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image 
+            src="/logo.png"
+            alt="logo"
+            height={1000}
+            width={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-center text-sm justify-between text-gray-500 mt-20">
+            <p className='justify-items-end '>Doctor Connect </p>
+            <Link href="/?admin=true" className="text-sm font-medium text-green-500">
+              Admin Panel
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src='/assets/images/onboarding-img.png'
+        height={1000}
+        width={1000}
+        className="side-img mx-w[50%]"
+        alt="onboarding"
+      />
     </div>
   );
 }

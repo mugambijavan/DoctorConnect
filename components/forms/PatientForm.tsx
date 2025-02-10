97b095063978a952cbd9b09233226bb15edf0e42
Javoh <117713938+mugambijavan@users.noreply.github.com>
2025-02-10 18:42:51 +0300

    Add form components and validation using react-hook-form and zod

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,9 +1,47 @@
-    import React from 'react';
-
+    "use client"
+    
+    import { zodResolver } from "@hookform/resolvers/zod"
+    import { useForm } from "react-hook-form"
+    import { z } from "zod"
+    import { Button } from "@/components/ui/button"
+    import { Form, } from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
+    
+    const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+    })
+    
     const PatientForm = () => {
-    return (
-        <div>PatientForm</div>
-    );
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+        username: "",
+        },
+    })
+    
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
     }
+    return (
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+            <section className="mb-12 space-y-4">
+                <h1 className="header">Hello There</h1>
+                <p className="text-green-500">Schedule your Doctors Appointment</p>
+            </section>
+            <CustomFormField 
+                control={form.control}
+            />
+            <Button type="submit">Submit</Button>
+        </form>
+        </Form>
+    )
+        }
 
-    export default PatientForm;
+        export default PatientForm;
